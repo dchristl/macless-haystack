a02bbda16fee9b17d97e8609ae285cd3a44b6d1a
niggowai <noreply@niggo.org>
2023-11-12 17:30:08 +0100

    Implemented the auth procedure by [biemster](https://github.com/biemster/FindMy) He uses a file generated by pypush which contains an access token, that in combination with an anisette server can perform the requests to icloud as well.
    A Mac/Iphone... is now only needed to generate the access token.

--- webserver/FindMy_proxy.py
+++ webserver/FindMy_proxy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import base64
 import json
-import six
 import ssl
 import sys
-
-from apple_cryptography import *
+import six
+import os
+import requests
+import datetime, time
 
 
 PORT = 6176
@@ -22,14 +22,14 @@ class ServerHandler(six.moves.SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_POST(self):
         if hasattr(self.headers, 'getheader'):
-          content_len = int(self.headers.getheader('content-length', 0))
+            content_len = int(self.headers.getheader('content-length', 0))
         else:
-          content_len = int(self.headers.get('content-length'))
+            content_len = int(self.headers.get('content-length'))
 
         post_body = self.rfile.read(content_len)
 
         print('Getting with post: ' + str(post_body))
-        UTCTime, Timezone, unixEpoch = getCurrentTimes()
+        UTCTime, Timezone, unixEpoch = self.getCurrentTimes()
         body = json.loads(post_body)
         if "days" in body:
             days = body['days']
@@ -37,32 +37,11 @@ class ServerHandler(six.moves.SimpleHTTPServer.SimpleHTTPRequestHandler):
             days = 7
         print('Querying for ' + str(days) + ' days')
         startdate = (unixEpoch - 60 * 60 * 24 * days) * 1000
-        data = '{"search": [{"ids": [\"%s\"]}]}' % ( "\",\"".join(body['ids']))
+        data = { "search": [{"startDate": startdate *1000, "endDate": unixEpoch *1000, "ids": body['ids']}] }
 
-        iCloud_decryptionkey = retrieveICloudKey()
-        AppleDSID, searchPartyToken = getAppleDSIDandSearchPartyToken(
-            iCloud_decryptionkey)
-        machineID, oneTimePassword = getOTPHeaders()
-        UTCTime, Timezone, unixEpoch = getCurrentTimes()
-        request_headers = {
-            'Authorization': "Basic %s" % (base64.b64encode((AppleDSID + ':' + searchPartyToken).encode('ascii')).decode('ascii')),
-            'X-Apple-I-MD': "%s" % (oneTimePassword),
-            'X-Apple-I-MD-RINFO': '17106176',
-            'X-Apple-I-MD-M': "%s" % (machineID),
-            'X-Apple-I-TimeZone': "%s" % (Timezone),
-            'X-Apple-I-Client-Time': "%s" % (UTCTime),
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-BA-CLIENT-TIMESTAMP': "%s" % (unixEpoch)
-        }
-
-        conn = six.moves.http_client.HTTPSConnection(
-            'gateway.icloud.com', timeout=5, context=ssl._create_unverified_context())
-        conn.request("POST", "/acsnservice/fetch", data, request_headers)
-        res = conn.getresponse()
-        result = json.loads(res.read())
-
-        results = result["results"]
+        r = requests.post("https://gateway.icloud.com/acsnservice/fetch", auth=self.getAuth(), headers=json.loads(requests.get('http://localhost:6969').text), json=data)
+        result = json.loads(r.content.decode())
+        results = result['results']
 
         newResults = [] 
         latestEntry = None
@@ -88,12 +67,26 @@ class ServerHandler(six.moves.SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         # print(responseBody)
 
+    def getCurrentTimes(self):
+        clientTime = datetime.datetime.utcnow().replace(microsecond=0).isoformat() + 'Z'
+        clientTimestamp = int(datetime.datetime.now().strftime('%s'))
+        return clientTime, time.tzname[1], clientTimestamp
+
+    def getAuth(self):
+        CONFIG_PATH = "../../pypush/config/openhaystack.json"
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH, "r") as f:
+                j = json.load(f)
+                return (j['ds_prs_id'], j['search_party_token'])
+        else:
+            print(f'No search-party-token found, please run pypush/examples/openhaystack.py as described in the README')
+            exit(1)
+
 
 
 if __name__ == "__main__":
     isV3 = sys.version_info.major > 2
     print('Using python3' if isV3 else 'Using python2')
-    retrieveICloudKey()
 
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
