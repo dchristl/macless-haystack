4fb8cd38d01ebb547a815a62c8ba2213718d1dcf
danny <smsoip@gmail.com>
2023-12-23 18:23:30 +0100

    some clarification

--- webserver/config.py
+++ webserver/config.py
@@ -7,8 +7,8 @@ CONFIG_FILE = "auth.json"
 CERT_FILE = "certificate.pem"  # optional
 KEY_FILE = "privkey.pem"  # optional
 LOG_LEVEL = logging.INFO
-USER = None
-PASS = None
+USER = None # if none/ empty ask for it
+PASS = None # if none/ empty ask for it
 
 
 def getConfigPath():
