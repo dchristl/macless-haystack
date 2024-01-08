55ca79405306191dbc59d0eb2ae0f41cdd43dd65
dchristl <smsoip@gmail.com>
2024-01-08 13:00:02 +0000

    Deploying to gh-pages from @ dchristl/macless-haystack@d5305c6b5c4f1d4bed90c0843314eccf0706cf04 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "8d28e99149c78ce99037cc8e61446753",
-"index.html": "c9741f28b23f25637194fa9d951751f7",
-"/": "c9741f28b23f25637194fa9d951751f7",
+"index.html": "884c885ea1e564cf87f5a0e1a6d5ab49",
+"/": "884c885ea1e564cf87f5a0e1a6d5ab49",
 "assets/AssetManifest.bin": "c9b50a909674afa4329338f0dff8d745",
 "assets/fonts/MaterialIcons-Regular.otf": "f79bfcf9536d68d6800a6f2a88c60b26",
 "assets/assets/OpenHaystackIcon.png": "31b03464dd04f04fcdee160f1d8582a4",
