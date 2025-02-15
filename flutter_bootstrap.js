5b5512e5c1af8b63d9600b5ba60d64f6de02e166
dchristl <smsoip@gmail.com>
2025-02-15 15:32:37 +0000

    Deploying to gh-pages from @ dchristl/macless-haystack@13faa33f77bfdca5711744f067b3207e2301ce2d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "118195325"
+    serviceWorkerVersion: "4054281951"
   }
 });
