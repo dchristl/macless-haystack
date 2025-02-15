6f9b189de35bcbb5534ca834553aa257c704f435
dchristl <smsoip@gmail.com>
2025-02-15 14:50:10 +0000

    Deploying to gh-pages from @ dchristl/macless-haystack@de930c15f0a73b6a01b7e7b22c153aeb23a220cb 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4021308344"
+    serviceWorkerVersion: "1656167367"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "b244236885291fe92013e71b0dc3a
 "icons/Icon-512.png": "b244236885291fe92013e71b0dc3a27e",
 "icons/Icon-maskable-192.png": "386e412d2cfa85e6d78e2ab4c9734d27",
 "icons/Icon-192.png": "386e412d2cfa85e6d78e2ab4c9734d27",
-"flutter_bootstrap.js": "8a84ef0b90faf0ffe9b4d1a56ea5d1b6",
+"flutter_bootstrap.js": "74fc0d466eb4e03faa1ca5cefbec8596",
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
