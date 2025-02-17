e553788c8a54552051fea620c5702b901b1467c2
danny <not@set.de>
2025-02-17 13:25:40 +0100

    readded unknown for existing tags

--- macless_haystack/lib/accessory/accessory_battery.dart
+++ macless_haystack/lib/accessory/accessory_battery.dart
@@ -2,5 +2,6 @@ enum AccessoryBatteryStatus {
   ok,     // Battery is currently charging
   medium,  // Battery is currently discharging
   low,         // Battery is fully charged
-  criticalLow    // Battery status is unknown or not applicable
+  criticalLow,    // Battery status is unknown or not applicable
+  unknown       // Battery status is unknown or not applicable
 }
