8f625787550ebbc4f5b440ada0347dce5232db16
danny <not@set.de>
2025-02-17 08:14:21 +0100

    Removed status check for supporting pix/heystack-nrf5x firmware #117

--- macless_haystack/lib/accessory/accessory_battery.dart
+++ macless_haystack/lib/accessory/accessory_battery.dart
@@ -2,6 +2,5 @@ enum AccessoryBatteryStatus {
   ok,     // Battery is currently charging
   medium,  // Battery is currently discharging
   low,         // Battery is fully charged
-  criticalLow,          // Battery level is low
-  unknown       // Battery status is unknown or not applicable
+  criticalLow    // Battery status is unknown or not applicable
 }
