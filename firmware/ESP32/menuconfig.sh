2491a5736ee262287ae29c9685c44080474897f2
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-06-12 02:35:14 -0400

    rework script to allow not only additive changes

--- firmware/ESP32/menuconfig.sh
+++ firmware/ESP32/menuconfig.sh
@@ -3,13 +3,30 @@
 # It runs menuconfig, but also respects changes already made with menuconfig
 
 # create a backup of the current sdkconfig
-cp sdkconfig.defaults sdkconfig.defaults.old
+mv sdkconfig.defaults sdkconfig.defaults.old
+# move potentially modified sdkconfig out of the way
+mv sdkconfig.esp32dev sdkconfig.esp32dev.new
+# generate a vanilla sdkconfig.esp32dev in a vacuum, with no modifications
+# by pressing 'q' repeatedly using the yes command, the interaction is skipped
+# (since there are no modifications, this will not create its own .old file)
+yes q | pio run -t menuconfig
+# move the new sdkconfig to a temporary file
+mv sdkconfig.esp32dev sdkconfig.esp32dev.vanilla || exit 1
+# restore potentially modified sdkconfig
+mv sdkconfig.esp32dev.new sdkconfig.esp32dev
+# restore original sdkconfig.defaults, while keeping the backup
+cp sdkconfig.defaults.old sdkconfig.defaults
 # user configures menuconfig
-pio run -t menuconfig
 # this will
 # 1. create sdkconfig.esp32dev based on sdkconfig.defaults and user input
-# 2. generate sdkconfig.esp32dev.old based on sdkconfig.defaults
+# 2 option a. generate sdkconfig.esp32dev.old based on sdkconfig.defaults
+# 2 option b. copy any existing sdkconfig.esp32dev to sdkconfig.esp32dev.old
+# 2 option c. create no sdkconfig.esp32dev.old if no changes were made and
+#             there is already an existing sdkconfig.esp32dev that was generated
+#             from sdkconfig.defaults with no modifications
+pio run -t menuconfig
 # generate new defaults with the modified sdkconfig
-grep -Fxv -f sdkconfig.esp32dev sdkconfig.esp32dev.old >> sdkconfig.defaults
+grep -Fxv -f sdkconfig.esp32dev.vanilla sdkconfig.esp32dev \
+    > sdkconfig.defaults || exit 1
 # we don't need sdkconfig.esp32dev.old, it's made redundant by our own .old file
-rm sdkconfig.esp32dev.old
+rm sdkconfig.esp32dev.old sdkconfig.esp32dev.vanilla
